0ca66cc9c86fe616e4706fcbe998bab6a8fd0d5a
mapradhan <139001582+mapradhan@users.noreply.github.com>
2024-07-10 17:48:31 +0530

    Update webpack.config.js

--- webpack.config.js
+++ webpack.config.js
@@ -108,49 +108,6 @@ module.exports = function(_env, argv) {
         )
       })
     ].filter(Boolean),
-    optimization: {
-      minimize: isProduction,
-      minimizer: [
-        new TerserWebpackPlugin({
-          terserOptions: {
-            compress: {
-              comparisons: false
-            },
-            mangle: {
-              safari10: true
-            },
-            output: {
-              comments: false,
-              ascii_only: true
-            },
-            warnings: false
-          }
-        }),
-        new OptimizeCssAssetsPlugin()
-      ],
-      splitChunks: {
-        chunks: "all",
-        minSize: 0,
-        maxInitialRequests: 10,
-        maxAsyncRequests: 10,
-        cacheGroups: {
-          vendors: {
-            test: /[\\/]node_modules[\\/]/,
-            name(module, chunks, cacheGroupKey) {
-              const packageName = module.context.match(
-                /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-              )[1];
-              return `${cacheGroupKey}.${packageName.replace("@", "")}`;
-            }
-          },
-          common: {
-            minChunks: 2,
-            priority: -10
-          }
-        }
-      },
-      runtimeChunk: "single"
-    },
     devServer: {
       compress: true,
       historyApiFallback: true,
